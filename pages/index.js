4f099ae24704ca8add24d4dc69b55e22bf5ae4e1
tozurichtogether@gmail.com <tozurichtogether@gmail.com>
2022-12-29 21:14:26 +0700

    Pisang Gorenk

--- pages/index.js
+++ pages/index.js
@@ -75,21 +75,7 @@ export default function Home() {
           <style>{`
       .hideMenuNav {
         display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 50vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
+      
     `}</style>
           <div className='text-center py-28 '>
             <h2 className='font-bold text-3xl text-blue-500'>Claudio Hans Figo</h2>
